cb18635bef089048e12ed812233657dd1656dd24
nicksebasco <nick.sebasco@ttu.edu>
2017-04-09 16:48:14 -0500

    Update csv.js

--- API/csv.js
+++ API/csv.js
@@ -4,6 +4,6 @@ module.exports = function (res,name,data){
   // other parameters contained in data: store_hours
   // map data object to an array of objects, needed for json2csv
   var fields = [ "name", "description", "phone", "website", "email", "street","city","zip","state"],
-  csv = json2csv({ data: data, fields: fields }),
+  csv = json2csv({ data: data, fields: fields });
   res.send(csv);
 };
